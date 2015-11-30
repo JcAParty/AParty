d560ad88a1ef2dff9123c991b0c91b6be0a9dde6
puacirl <1558391434@qq.com>
2015-11-30 18:58:17 +0800

    11月24

---
+++ AParty 3/AParty/C/FindPasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  FindPasswordViewController.h
+//  AParty
+//
+//  Created by puacirl on 15/11/25.
+//  Copyright © 2015年 JConnor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FindPasswordViewController : UIViewController
+
+@end
---
+++ AParty 3/AParty/C/ResetPasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResetPasswordViewController.h
+//  AParty
+//
+//  Created by puacirl on 15/11/25.
+//  Copyright © 2015年 JConnor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResetPasswordViewController : UIViewController
+
+@end
